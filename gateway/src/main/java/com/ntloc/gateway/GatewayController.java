c24fe9b079d5b37250810c472f3dbe7ac61235e8
APAC\AOY1HC <loc.nguyenthanh2@vn.bosch.vn>
2022-12-03 22:00:02 +0700

    circuit-breaker-update

--- gateway/src/main/java/com/ntloc/gateway/GatewayController.java
+++ gateway/src/main/java/com/ntloc/gateway/GatewayController.java
@@ -6,23 +6,26 @@ import org.springframework.web.bind.annotation.RestController;
 
 import static org.springframework.web.bind.annotation.RequestMethod.*;
 
-//@Slf4j
-//@RestController
-//@RequestMapping(path = "/fallback")
+@Slf4j
+@RestController
+@RequestMapping(path = "/fallback")
 public class GatewayController {
 
-//    @RequestMapping(path = "/customer", method = {GET, POST, DELETE, PUT})
-//    public String customerFallback() {
-//        return "Customer service unavailable !";
-//    }
-//
-//    @RequestMapping(path = "/product", method = {GET, POST, DELETE, PUT})
-//    public String productFallback() {
-//        return "Product service unavailable !";
-//    }
-//
-//    @RequestMapping(path = "/orders", method = {GET, POST, DELETE, PUT})
-//    public String ordersFallback() {
-//        return "Orders service unavailable !";
-//    }
+    @RequestMapping(path = "/customer", method = {GET, POST, DELETE, PUT})
+    public String customerFallback() {
+        log.info("Customer service fallback call");
+        return "Customer service unavailable !";
+    }
+
+    @RequestMapping(path = "/product", method = {GET, POST, DELETE, PUT})
+    public String productFallback() {
+        log.info("Product service fallback call");
+        return "Product service unavailable !";
+    }
+
+    @RequestMapping(path = "/orders", method = {GET, POST, DELETE, PUT})
+    public String ordersFallback() {
+        log.info("Orders service fallback call");
+        return "Orders service unavailable !";
+    }
 }
