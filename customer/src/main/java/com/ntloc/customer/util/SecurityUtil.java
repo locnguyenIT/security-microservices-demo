0a6fe8b8bc78b7794676fd638badf192190334d8
ntloc <ntloc.developer@gmail.com>
2022-12-09 22:27:03 +0700

    security-change-jwt-to-jwt-authentication-token

--- customer/src/main/java/com/ntloc/customer/util/SecurityUtil.java
+++ customer/src/main/java/com/ntloc/customer/util/SecurityUtil.java
@@ -5,8 +5,10 @@ import com.ntloc.customer.CustomerRepository;
 import com.ntloc.exception.NotFoundException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.authentication.AuthenticationCredentialsNotFoundException;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
 
 import static com.ntloc.customer.CustomerConstant.*;
 
@@ -14,11 +16,12 @@ import static com.ntloc.customer.CustomerConstant.*;
 public class SecurityUtil {
 
     public static String getCurrentUsername() {
-        Jwt jwt = (Jwt) SecurityContextHolder.getContext().getAuthentication();
-        if (jwt == null) {
+        JwtAuthenticationToken jwtAuthenticationToken = (JwtAuthenticationToken) SecurityContextHolder.getContext().getAuthentication();
+//        Jwt jwt = (Jwt) SecurityContextHolder.getContext().getAuthentication();
+        if (jwtAuthenticationToken == null) {
             throw new AuthenticationCredentialsNotFoundException(CREDENTIAL_NOT_FOUND);
         }
-        String preferred_username = jwt.getClaims().get(PREFERRED_USERNAME).toString();
+        String preferred_username = jwtAuthenticationToken.getTokenAttributes().get(PREFERRED_USERNAME).toString();
         return preferred_username;
     }
 
