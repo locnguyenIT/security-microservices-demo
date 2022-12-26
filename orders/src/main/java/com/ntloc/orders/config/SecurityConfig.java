a6ab61af94c15dabde663931c60c9aeb14750105
APAC\AOY1HC <loc.nguyenthanh2@vn.bosch.vn>
2022-12-26 22:08:13 +0700

    security-setting-orders

--- orders/src/main/java/com/ntloc/orders/config/SecurityConfig.java
+++ orders/src/main/java/com/ntloc/orders/config/SecurityConfig.java
@@ -1,13 +1,18 @@
 package com.ntloc.orders.config;
 
+import com.ntloc.exception.CustomOAuth2AccessDeniedHandler;
+import com.ntloc.exception.CustomOAuth2AuthenticationHandler;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig {
 
     @Bean
@@ -19,8 +24,19 @@ public class SecurityConfig {
                 .anyRequest()
                 .authenticated()
                 .and()
-                .oauth2ResourceServer(oauth2 -> oauth2.jwt())
+                .oauth2ResourceServer(oauth2 -> {
+                    oauth2.jwt().jwtAuthenticationConverter(jwtAuthenticationConverter());
+                    oauth2.authenticationEntryPoint(new CustomOAuth2AuthenticationHandler());
+                    oauth2.accessDeniedHandler(new CustomOAuth2AccessDeniedHandler());
+                })
                 .build();
+    }
+
+    @Bean
+    public JwtAuthenticationConverter jwtAuthenticationConverter() {
+        JwtAuthenticationConverter jwtAuthenticationConverter = new JwtAuthenticationConverter();
+        jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(new JWTGrantedAuthorityConverter());
+        return jwtAuthenticationConverter;
     }
 
 }
